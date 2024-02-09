f15f66ed9c7a4b8050f49a078653f48bf1c121a8
AGV <agonzalez.venegas@outlook.com>
2024-02-09 20:51:16 +0100

    Clase Calculadora OK

---
+++ src/sockets/Calculadora.java
@@ -0,0 +1,20 @@
+package sockets;
+
+public class Calculadora {
+	
+	public double Suma(double a, double b) {
+		return (a + b);
+	}
+
+	public double Resta(double a, double b) {
+		return (a - b);
+	}
+	
+	public double Multiplicacion(double a, double b) {
+		return (a * b);
+	}
+	
+	public double Division(double a, double b) throws ArithmeticException {
+		return (a / b);
+	}
+}
